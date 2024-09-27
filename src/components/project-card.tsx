1992f85413fcca91b0a895ee57a8fa464bb3f359
Kemal Zulfiqar <kemalzulfiqar@Kemals-MacBook-M1-Pro.local>
2024-09-27 15:16:11 +0700

    remove iframe laxi load

--- src/components/project-card.tsx
+++ src/components/project-card.tsx
@@ -56,109 +56,102 @@ export function ProjectCard({
   };
 
   return (
-    <>
-      <Card
-        className={
-          "flex flex-col overflow-hidden border hover:shadow-lg transition-all duration-300 ease-out h-full"
-        }
-      >
-        <div className={cn("relative block cursor-pointer", className)}>
-          {video && (
-            <video
-              src={video}
-              autoPlay
-              loop
-              muted
-              playsInline
-              className="pointer-events-none mx-auto h-40 w-full object-cover object-top" // needed because random black line at bottom of video
-            />
-          )}
-          {image && (
-            <>
-              <Image
-                className="w-full object-cover aspect-video hover:object-contain"
-                preview={{
-                  mask: (
-                    <div className="flex items-center gap-x-1 text-sm">
-                      Show Deck <ArrowRight size={14} />
-                    </div>
-                  ),
-                  destroyOnClose: true,
-                  onVisibleChange: (visible) => {
-                    if (visible === false) {
-                      setLoading(true);
-                    }
-                  },
-                  imageRender: () => (
-                    <>
-                      <iframe
-                        className={`w-full aspect-video max-w-[1080px] ${
-                          loading && "hidden"
-                        }`}
-                        loading="lazy"
-                        // style="position: absolute; width: 100%; height: 100%; top: 0; left: 0; border: none; padding: 0;margin: 0;"
-                        src={iframe}
-                        onLoad={() => setLoading(false)}
-                        allow="fullscreen"
-                      />
+    <Card
+      className={
+        "flex flex-col overflow-hidden border hover:shadow-lg transition-all duration-300 ease-out h-full"
+      }
+    >
+      <div className={cn("relative block cursor-pointer", className)}>
+        {video && (
+          <video
+            src={video}
+            autoPlay
+            loop
+            muted
+            playsInline
+            className="pointer-events-none mx-auto h-40 w-full object-cover object-top" // needed because random black line at bottom of video
+          />
+        )}
+        {image && (
+          <Image
+            className="w-full object-cover aspect-video hover:object-contain"
+            preview={{
+              mask: (
+                <div className="flex items-center gap-x-1 text-sm">
+                  Show Deck <ArrowRight size={14} />
+                </div>
+              ),
+              destroyOnClose: true,
+              onVisibleChange: (visible) => {
+                if (visible === false) {
+                  setLoading(true);
+                }
+              },
+              imageRender: () => (
+                <>
+                  <iframe
+                    className={`w-full aspect-video max-w-[1080px] ${
+                      loading && "hidden"
+                    }`}
+                    // loading="lazy"
+                    // style="position: absolute; width: 100%; height: 100%; top: 0; left: 0; border: none; padding: 0;margin: 0;"
+                    src={iframe}
+                    onLoad={() => setLoading(false)}
+                    allow="fullscreen"
+                  />
 
-                      {loading && (
-                        <p className="!text-white text-xl">Loading...</p>
-                      )}
-                    </>
-                  ),
-                  toolbarRender: () => null,
-                }}
-                src={image}
-              />
-            </>
-          )}
-        </div>
-        <CardHeader className="px-2">
-          <div className="space-y-1">
-            <CardTitle className="mt-1 text-base">{title}</CardTitle>
-            <time className="font-sans text-xs">{dates}</time>
-            <div className="hidden font-sans text-xs underline print:visible">
-              {link
-                ?.replace("https://", "")
-                .replace("www.", "")
-                .replace("/", "")}
-            </div>
-            <Markdown className="prose max-w-full text-pretty font-sans text-xs text-muted-foreground dark:prose-invert">
-              {description}
-            </Markdown>
+                  {loading && (
+                    <p className="!text-white text-base ">Loading...</p>
+                  )}
+                </>
+              ),
+              toolbarRender: () => null,
+            }}
+            src={image}
+          />
+        )}
+      </div>
+      <CardHeader className="px-2">
+        <div className="space-y-1">
+          <CardTitle className="mt-1 text-base">{title}</CardTitle>
+          <time className="font-sans text-xs">{dates}</time>
+          <div className="hidden font-sans text-xs underline print:visible">
+            {link?.replace("https://", "").replace("www.", "").replace("/", "")}
           </div>
-        </CardHeader>
-        <CardContent className="mt-auto flex flex-col px-2">
-          {tags && tags.length > 0 && (
-            <div className="mt-2 flex flex-wrap gap-1">
-              {tags?.map((tag) => (
-                <Badge
-                  className="px-1 py-0 text-[10px]"
-                  variant="secondary"
-                  key={tag}
-                >
-                  {tag}
+          <Markdown className="prose max-w-full text-pretty font-sans text-xs text-muted-foreground dark:prose-invert">
+            {description}
+          </Markdown>
+        </div>
+      </CardHeader>
+      <CardContent className="mt-auto flex flex-col px-2">
+        {tags && tags.length > 0 && (
+          <div className="mt-2 flex flex-wrap gap-1">
+            {tags?.map((tag) => (
+              <Badge
+                className="px-1 py-0 text-[10px]"
+                variant="secondary"
+                key={tag}
+              >
+                {tag}
+              </Badge>
+            ))}
+          </div>
+        )}
+      </CardContent>
+      <CardFooter className="px-2 pb-2">
+        {links && links.length > 0 && (
+          <div className="flex flex-row flex-wrap items-start gap-1">
+            {links?.map((link, idx) => (
+              <Link href={link?.href} key={idx} target="_blank">
+                <Badge key={idx} className="flex gap-2 px-2 py-1 text-[10px]">
+                  {link.icon}
+                  {link.type} <ArrowUpRight size={12} />
                 </Badge>
-              ))}
-            </div>
-          )}
-        </CardContent>
-        <CardFooter className="px-2 pb-2">
-          {links && links.length > 0 && (
-            <div className="flex flex-row flex-wrap items-start gap-1">
-              {links?.map((link, idx) => (
-                <Link href={link?.href} key={idx} target="_blank">
-                  <Badge key={idx} className="flex gap-2 px-2 py-1 text-[10px]">
-                    {link.icon}
-                    {link.type} <ArrowUpRight size={12} />
-                  </Badge>
-                </Link>
-              ))}
-            </div>
-          )}
-        </CardFooter>
-      </Card>
-    </>
+              </Link>
+            ))}
+          </div>
+        )}
+      </CardFooter>
+    </Card>
   );
 }
